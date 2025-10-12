759d3ace4a4e4cd4d7d5e5754df399a228eb463a
gulferyigit <gulferyigit65@gmail.com>
2025-10-12 17:44:08 +0300

    Initial setup and project files

---
+++ app/src/main/java/gulfer/emine/diary/SecondFragment.java
@@ -0,0 +1,48 @@
+package gulfer.emine.diary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import gulfer.emine.diary.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Eski navigasyon kodu buradaydı. Şimdi yerine Toast mesajı yazıyoruz:
+                Toast.makeText(getActivity(), "Butona Basıldı!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
